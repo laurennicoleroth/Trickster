7357a0645043e845febbb557ed98e93d6d98bd20
Lauren N. Roth <laurennicoleroth@gmail.com>
2017-12-01 15:27:05 -0500

    reset controller code

--- Trickster/Controller/ViewController.swift
+++ Trickster/Controller/ViewController.swift
@@ -16,74 +16,65 @@ class ViewController: UIViewController {
   @IBOutlet var scoreLabel: UILabel!
   @IBOutlet var progressView: UIView!
   
-  var tricks = TrickBank(trickDifficulty: 1)
-  var score : Int = 0
-  var trickNumber : Int = 0
+  //Place your instance variables here
   
+  let tricks = TrickBank(trickDifficulty: 1)
+  var trickNumber : Int = 0
+  var score : Int = 0
+  
+  
+  // This gets called when the UIViewController is loaded into memory when the app starts
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    trickNumberLabel.text = "\(trickNumber + 1) / \(tricks.list.count)"
-    trickNameLabel.text = tricks.list[trickNumber].name.uppercased()
-    demoImage.image = UIImage(named: tricks.list[trickNumber].image)
-     progressView.frame.size.width = (view.frame.size.width / CGFloat(tricks.list.count)) * CGFloat(trickNumber)
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-
-  }
-
-  @IBAction func trickCompletedButtonPressed(_ sender: UIButton) {
-    
-    switch sender.tag {
-    case 1:
-      print("Good dog!")
-      tricks.list[trickNumber].successCount += 1
-      score += 1
-      ProgressHUD.showSuccess("Good dog!!")
-    case 2:
-      print("Try again!")
-      tricks.list[trickNumber].successCount -= 1
-    default:
-      print("oops")
-    }
-    
-    checkTrickScore()
     
     updateUI()
     
   }
   
-  func checkTrickScore() {
-    print("Success on this trick: ", tricks.list[trickNumber].successCount)
-    
-    if trickNumber < tricks.list.count - 1 {
-      trickNumber += 1
-    } else {
-      print("Ask user if they want to try another trick")
-    }
+  // Nothing to modify here. This gets called when the system is low on memory
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
   }
   
+  //This action gets called when either the true or false button is pressed.
+  @IBAction func trickCompletedButtonPressed(_ sender: AnyObject) {
+    
+    if sender.tag == 1 {
+//      pickedAnswer = true
+    }
+      
+    else if sender.tag == 2 {
+//      pickedAnswer = false
+    }
+    
+    checkAnswer()
+    
+    trickNumber = trickNumber + 1
+    
+    updateUI()
+    
+  }
+  
+  // This method will update all the views on screen (progress bar, score label, etc)
   func updateUI() {
     
-    trickNameLabel.text = tricks.list[trickNumber].name.uppercased()
-    demoImage.image = UIImage(named: tricks.list[trickNumber].image)
-    trickNumberLabel.text = "\(trickNumber + 1) / \(tricks.list.count)"
-    scoreLabel.text = "\(score)"
-    progressView.frame.size.width = (view.frame.size.width / CGFloat(tricks.list.count)) * CGFloat(trickNumber)
+    progressView.frame.size.width = (view.frame.size.width / 13) * CGFloat(trickNumber)
     
-//    progressLabel.text = String(trickNumber) + "\(tricks.list.count)"
+    trickNumberLabel.text = String(trickNumber) + "/13"
     
-    nextTrick()
+    scoreLabel.text = "Score: " + String(score)
+    
+    nextQuestion()
   }
   
-  func nextTrick() {
-    if trickNumber < tricks.list.count {
+  //This method will update the question text and check if we reached the end.
+  func nextQuestion() {
+    
+    if trickNumber <= tricks.list.count - 1 {
       trickNameLabel.text = tricks.list[trickNumber].name
     }
     else {
-      let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+      let alert = UIAlertController(title: "Awesome", message: "You've finished all the tricks, do you want to start over?", preferredStyle: .alert)
       
       let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { UIAlertAction in
         self.startOver()
@@ -93,10 +84,33 @@ class ViewController: UIViewController {
       
       present(alert, animated: true, completion: nil)
     }
+    
   }
   
+  //This method will check if the user has got the right answer.
+  func checkAnswer() {
+    
+    let successCount = tricks.list[trickNumber].successCount
+    
+    if successCount > 0 {
+      
+      ProgressHUD.showSuccess("Good Dog!")
+      
+      score = score + 1
+    }
+    else {
+      
+      ProgressHUD.showError("Try again!")
+    }
+  }
   
+  //This method will wipe the board clean, so that users can retake the quiz.
   func startOver() {
+    
+    trickNumber = 0
+    score = 0
+    
+    updateUI()
     
   }
   
